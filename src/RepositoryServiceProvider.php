4b90db32e700d3661a282ade89fc8c93d2287c24
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-11-01 01:20:34 +0600

    missing RepositoryServiceProvider typo fixed

--- src/RepositoryServiceProvider.php
+++ src/RepositoryServiceProvider.php
@@ -13,7 +13,7 @@ class RepositoryServiceProvider extends ServiceProvider implements DeferrablePro
      */
     public function register(): void
     {
-        foreach (Config::get('fintech.Remit.repositories', []) as $interface => $binding) {
+        foreach (Config::get('fintech.remit.repositories', []) as $interface => $binding) {
             $this->app->bind($interface, function ($app) use ($binding) {
                 return $app->make($binding);
             });
@@ -27,6 +27,6 @@ class RepositoryServiceProvider extends ServiceProvider implements DeferrablePro
      */
     public function provides(): array
     {
-        return array_keys(Config::get('fintech.Remit.repositories', []));
+        return array_keys(Config::get('fintech.remit.repositories', []));
     }
 }
