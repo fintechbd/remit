b8b502ac8b986087956a64baa53d173cfa5680b5
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-11-04 14:46:40 +0600

    facade class doc support added

--- src/Facades/Remit.php
+++ src/Facades/Remit.php
@@ -5,6 +5,8 @@ namespace Fintech\Remit\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * // Crud Service Method Point Do not Remove //
+ *
  * @see \Fintech\Remit\Remit
  */
 class Remit extends Facade
