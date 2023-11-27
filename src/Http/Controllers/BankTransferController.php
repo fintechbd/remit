b5a5e16139dfa1ba885149c75121e8ebb9423d45
mah-shamim <mah-shamim@users.noreply.github.com>
2023-11-27 20:38:12 +0000

    Fix styling

--- src/Http/Controllers/BankTransferController.php
+++ src/Http/Controllers/BankTransferController.php
@@ -9,7 +9,6 @@ use Fintech\Core\Enums\Auth\RiskProfile;
 use Fintech\Core\Enums\Auth\SystemRole;
 use Fintech\Core\Enums\Reload\DepositStatus;
 use Fintech\Core\Enums\Transaction\OrderStatus;
-use Fintech\Core\Enums\Transaction\OrderStatusConfig;
 use Fintech\Core\Exceptions\DeleteOperationException;
 use Fintech\Core\Exceptions\RestoreOperationException;
 use Fintech\Core\Exceptions\StoreOperationException;
