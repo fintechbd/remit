982f8b9ec8036b399f257e2fc3ad842429746631
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-11-29 04:20:37 +0600

    LP-158 store done

--- src/Http/Controllers/WalletTransferController.php
+++ src/Http/Controllers/WalletTransferController.php
@@ -2,6 +2,12 @@
 
 namespace Fintech\Remit\Http\Controllers;
 use Exception;
+use Fintech\Banco\Facades\Banco;
+use Fintech\Business\Facades\Business;
+use Fintech\Core\Enums\Auth\RiskProfile;
+use Fintech\Core\Enums\Auth\SystemRole;
+use Fintech\Core\Enums\Reload\DepositStatus;
+use Fintech\Core\Enums\Transaction\OrderStatus;
 use Fintech\Core\Exceptions\StoreOperationException;
 use Fintech\Core\Exceptions\UpdateOperationException;
 use Fintech\Core\Exceptions\DeleteOperationException;
@@ -73,12 +79,74 @@ class WalletTransferController extends Controller
         try {
             $inputs = $request->validated();
 
+            if ($request->input('user_id') > 0) {
+                $user_id = $request->input('user_id');
+            }
+            $depositor = $request->user('sanctum');
+
+            $depositAccount = \Fintech\Transaction\Facades\Transaction::userAccount()->list([
+                'user_id' => $user_id ?? $depositor->getKey(),
+                'country_id' => $request->input('source_country_id', $depositor->profile?->country_id),
+            ])->first();
+
+            if (! $depositAccount) {
+                throw new Exception("User don't have account deposit balance");
+            }
+
+            $masterUser = \Fintech\Auth\Facades\Auth::user()->list([
+                'role_name' => SystemRole::MasterUser->value,
+                'country_id' => $request->input('source_country_id', $depositor->profile?->country_id),
+            ])->first();
+
+            if (! $masterUser) {
+                throw new Exception('Master User Account not found for '.$request->input('source_country_id', $depositor->profile?->country_id).' country');
+            }
+
+            //set pre defined conditions of deposit
             $inputs['transaction_form_id'] = Transaction::transactionForm()->list(['code' => 'wallet_transfer'])->first()->getKey();
+            $inputs['user_id'] = $user_id ?? $depositor->getKey();
+            $delayCheck = Transaction::order()->transactionDelayCheck($inputs);
+            if ($delayCheck['countValue'] > 0) {
+                throw new Exception('Your Request For This Amount Is Already Submitted. Please Wait For Update');
+            }
+            $inputs['sender_receiver_id'] = $masterUser->getKey();
+            $inputs['is_refunded'] = false;
+            $inputs['status'] = OrderStatus::Successful->value;
+            $inputs['risk'] = RiskProfile::Low->value;
+            //TODO CONVERTER
+            $inputs['converted_amount'] = $inputs['amount'];
+            $inputs['converted_currency'] = $inputs['currency'];
+            $inputs['order_data']['created_by'] = $depositor->name;
+            $inputs['order_data']['created_by_mobile_number'] = $depositor->mobile;
+            $inputs['order_data']['created_at'] = now();
+            $inputs['order_data']['master_user_name'] = $masterUser['name'];
+            //$inputs['order_data']['operator_short_code'] = $request->input('operator_short_code', null);
+            $inputs['order_data']['assign_order'] = 'no';
+            $inputs['order_data']['system_notification_variable_success'] = 'wallet_transfer_success';
+            $inputs['order_data']['system_notification_variable_failed'] = 'wallet_transfer_failed';
+
             $walletTransfer = Remit::walletTransfer()->create($inputs);
 
             if (!$walletTransfer) {
                 throw (new StoreOperationException)->setModel(config('fintech.remit.wallet_transfer_model'));
             }
+
+            $order_data = $walletTransfer->order_data;
+            $order_data['purchase_number'] = entry_number($walletTransfer->getKey(), $walletTransfer->sourceCountry->iso3, OrderStatus::Successful->value);
+            $order_data['service_stat_data'] = Business::serviceStat()->serviceStateData($walletTransfer);
+            $order_data['user_name'] = $walletTransfer->user->name;
+            Remit::walletTransfer()->debitTransaction($walletTransfer);
+            $depositedAccount = \Fintech\Transaction\Facades\Transaction::userAccount()->list([
+                'user_id' => $depositor->getKey(),
+                'country_id' => $walletTransfer->source_country_id,
+            ])->first();
+            $order_data['order_data']['previous_amount'] = $depositedAccount->user_account_data['available_amount'];
+            $order_data['order_data']['current_amount'] = ($order_data['order_data']['previous_amount'] + $inputs['amount']);
+            //TODO ALL Beneficiary Data with bank and branch data
+            $beneficiaryData = Banco::beneficiary()->manageBeneficiaryData($order_data);
+            $order_data['order_data']['beneficiary_data'] = $beneficiaryData;
+
+            Remit::walletTransfer()->update($walletTransfer->getKey(), ['order_data' => $order_data, 'order_number' => $order_data['purchase_number']]);
 
             return $this->created([
                 'message' => __('core::messages.resource.created', ['model' => 'Wallet Transfer']),
