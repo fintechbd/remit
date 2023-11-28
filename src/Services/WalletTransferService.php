bbdcfb7725e0d5f15d4570fed1b8cab49ecc3fd2
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-11-29 04:20:16 +0600

    LP-158 debit and credit method done

--- src/Services/WalletTransferService.php
+++ src/Services/WalletTransferService.php
@@ -4,9 +4,14 @@ namespace Fintech\Remit\Services;
 
 
 use Fintech\Remit\Interfaces\WalletTransferRepository;
+use Fintech\Transaction\Facades\Transaction;
+use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 
 /**
  * Class WalletTransferService
+ * @property WalletTransferRepository $walletTransferRepository
  * @package Fintech\Remit\Services
  *
  */
@@ -22,46 +27,229 @@ class WalletTransferService
 
     /**
      * @param array $filters
-     * @return mixed
+     * @return Collection|Paginator
      */
-    public function list(array $filters = [])
+    public function list(array $filters = []): Collection|Paginator
     {
         return $this->walletTransferRepository->list($filters);
 
     }
 
-    public function create(array $inputs = [])
+    /**
+     * @param array $inputs
+     * @return Model|\MongoDB\Laravel\Eloquent\Model|null
+     */
+    public function create(array $inputs = []): Model|\MongoDB\Laravel\Eloquent\Model|null
     {
         return $this->walletTransferRepository->create($inputs);
     }
 
-    public function find($id, $onlyTrashed = false)
+    /**
+     * @param $id
+     * @param bool $onlyTrashed
+     * @return Model|\MongoDB\Laravel\Eloquent\Model|null
+     */
+    public function find($id, bool $onlyTrashed = false): Model|\MongoDB\Laravel\Eloquent\Model|null
     {
         return $this->walletTransferRepository->find($id, $onlyTrashed);
     }
 
-    public function update($id, array $inputs = [])
+    /**
+     * @param $id
+     * @param array $inputs
+     * @return Model|\MongoDB\Laravel\Eloquent\Model|null
+     */
+    public function update($id, array $inputs = []): Model|\MongoDB\Laravel\Eloquent\Model|null
     {
         return $this->walletTransferRepository->update($id, $inputs);
     }
 
-    public function destroy($id)
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function destroy($id): mixed
     {
         return $this->walletTransferRepository->delete($id);
     }
 
-    public function restore($id)
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function restore($id): mixed
     {
         return $this->walletTransferRepository->restore($id);
     }
 
-    public function export(array $filters)
+    /**
+     * @param array $filters
+     * @return Paginator|Collection
+     */
+    public function export(array $filters): Paginator|Collection
     {
         return $this->walletTransferRepository->list($filters);
     }
 
-    public function import(array $filters)
+    /**
+     * @param array $filters
+     * @return Model|\MongoDB\Laravel\Eloquent\Model|null
+     */
+    public function import(array $filters): Model|\MongoDB\Laravel\Eloquent\Model|null
     {
         return $this->walletTransferRepository->create($filters);
     }
+
+    /**
+     * @return int[]
+     */
+    public function debitTransaction($data): array
+    {
+        $serviceStatData = $data->order_data['service_stat_data'];
+        $master_user_name = $data->order_data['master_user_name'];
+        $user_name = $data->order_data['user_name'];
+
+        $amount = $data->amount;
+        $converted_amount = $data->converted_amount;
+        $data->amount = -$amount;
+        $data->converted_amount = -$converted_amount;
+        $data->order_detail_cause_name = 'cash_withdraw';
+        $data->order_detail_number = $data->order_data['purchase_number'];
+        $data->order_detail_response_id = $data->order_data['purchase_number'];
+        $data->notes = 'Wallet Transfer Payment Send to '.$master_user_name;
+        $orderDetailStore = Transaction::orderDetail()->create(Transaction::orderDetail()->orderDetailsDataArrange($data));
+        $orderDetailStore->order_detail_parent_id = $data->order_detail_parent_id = $orderDetailStore->getKey();
+        $orderDetailStore->save();
+        $orderDetailStore->fresh();
+        $orderDetailStoreForMaster = $orderDetailStore->replicate();
+        $orderDetailStoreForMaster->user_id = $data->sender_receiver_id;
+        $orderDetailStoreForMaster->sender_receiver_id = $data->user_id;
+        $orderDetailStoreForMaster->order_detail_amount = $amount;
+        $orderDetailStoreForMaster->converted_amount = $converted_amount;
+        $orderDetailStoreForMaster->step = 2;
+        $orderDetailStoreForMaster->notes = 'Wallet Transfer Payment Receive From'.$user_name;
+        $orderDetailStoreForMaster->save();
+
+        //For Charge
+        $data->amount = calculate_flat_percent($amount, $serviceStatData['charge']);
+        $data->converted_amount = calculate_flat_percent($converted_amount, $serviceStatData['charge']);
+        $data->order_detail_cause_name = 'charge';
+        $data->order_detail_parent_id = $orderDetailStore->getKey();
+        $data->notes = 'Wallet Transfer Charge Send to '.$master_user_name;
+        $data->step = 3;
+        $data->order_detail_parent_id = $orderDetailStore->getKey();
+        $orderDetailStoreForCharge = Transaction::orderDetail()->create(Transaction::orderDetail()->orderDetailsDataArrange($data));
+        $orderDetailStoreForChargeForMaster = $orderDetailStoreForCharge->replicate();
+        $orderDetailStoreForChargeForMaster->user_id = $data->sender_receiver_id;
+        $orderDetailStoreForChargeForMaster->sender_receiver_id = $data->user_id;
+        $orderDetailStoreForChargeForMaster->order_detail_amount = -calculate_flat_percent($amount, $serviceStatData['charge']);
+        $orderDetailStoreForChargeForMaster->converted_amount = -calculate_flat_percent($converted_amount, $serviceStatData['charge']);
+        $orderDetailStoreForChargeForMaster->order_detail_cause_name = 'charge';
+        $orderDetailStoreForChargeForMaster->notes = 'Wallet Transfer Charge Receive from '.$user_name;
+        $orderDetailStoreForChargeForMaster->step = 4;
+        $orderDetailStoreForChargeForMaster->save();
+
+        //For Discount
+        $data->amount = -calculate_flat_percent($amount, $serviceStatData['discount']);
+        $data->converted_amount = -calculate_flat_percent($converted_amount, $serviceStatData['discount']);
+        $data->order_detail_cause_name = 'discount';
+        $data->notes = 'Wallet Transfer Discount form '.$master_user_name;
+        $data->step = 5;
+        //$data->order_detail_parent_id = $orderDetailStore->getKey();
+        $updateData['order_data']['previous_amount'] = 0;
+        $orderDetailStoreForDiscount = Transaction::orderDetail()->create(Transaction::orderDetail()->orderDetailsDataArrange($data));
+        $orderDetailStoreForDiscountForMaster = $orderDetailStoreForCharge->replicate();
+        $orderDetailStoreForDiscountForMaster->user_id = $data->sender_receiver_id;
+        $orderDetailStoreForDiscountForMaster->sender_receiver_id = $data->user_id;
+        $orderDetailStoreForDiscountForMaster->order_detail_amount = calculate_flat_percent($amount, $serviceStatData['discount']);
+        $orderDetailStoreForDiscountForMaster->converted_amount = calculate_flat_percent($converted_amount, $serviceStatData['discount']);
+        $orderDetailStoreForDiscountForMaster->order_detail_cause_name = 'discount';
+        $orderDetailStoreForDiscountForMaster->notes = 'Wallet Transfer Discount to '.$user_name;
+        $orderDetailStoreForDiscountForMaster->step = 6;
+        $orderDetailStoreForDiscountForMaster->save();
+
+        //'Point Transfer Commission Send to ' . $masterUser->name;
+        //'Point Transfer Commission Receive from ' . $receiver->name;
+        return [
+            'previous_amount' => 0,
+            'current_amount' => 0,
+        ];
+
+    }
+
+    /**
+     * @return int[]
+     */
+    public function creditTransaction($data): array
+    {
+        $serviceStatData = $data->order_data['service_stat_data'];
+        $master_user_name = $data->order_data['master_user_name'];
+        $user_name = $data->order_data['user_name'];
+
+        $data->order_detail_cause_name = 'cash_withdraw';
+        $data->order_detail_number = $data->order_data['accepted_number'];
+        $data->order_detail_response_id = $data->order_data['purchase_number'];
+        $data->notes = 'Wallet Transfer Refund From '.$master_user_name;
+        $orderDetailStore = Transaction::orderDetail()->create(Transaction::orderDetail()->orderDetailsDataArrange($data));
+        $orderDetailStore->order_detail_parent_id = $data->order_detail_parent_id = $orderDetailStore->getKey();
+        $orderDetailStore->save();
+        $orderDetailStore->fresh();
+        $amount = $data->amount;
+        $converted_amount = $data->converted_amount;
+        $orderDetailStoreForMaster = $orderDetailStore->replicate();
+        $orderDetailStoreForMaster->user_id = $data->sender_receiver_id;
+        $orderDetailStoreForMaster->sender_receiver_id = $data->user_id;
+        $orderDetailStoreForMaster->order_detail_amount = -$amount;
+        $orderDetailStoreForMaster->converted_amount = -$converted_amount;
+        $orderDetailStoreForMaster->step = 2;
+        $orderDetailStoreForMaster->notes = 'Wallet Transfer Send to '.$user_name;
+        $orderDetailStoreForMaster->save();
+
+        //For Charge
+        $data->amount = -calculate_flat_percent($amount, $serviceStatData['charge']);
+        $data->converted_amount = -calculate_flat_percent($converted_amount, $serviceStatData['charge']);
+        $data->order_detail_cause_name = 'charge';
+        $data->order_detail_parent_id = $orderDetailStore->getKey();
+        $data->notes = 'Wallet Transfer Charge Receive from '.$master_user_name;
+        $data->step = 3;
+        $data->order_detail_parent_id = $orderDetailStore->getKey();
+        $orderDetailStoreForCharge = Transaction::orderDetail()->create(Transaction::orderDetail()->orderDetailsDataArrange($data));
+        $orderDetailStoreForChargeForMaster = $orderDetailStoreForCharge->replicate();
+        $orderDetailStoreForChargeForMaster->user_id = $data->sender_receiver_id;
+        $orderDetailStoreForChargeForMaster->sender_receiver_id = $data->user_id;
+        $orderDetailStoreForChargeForMaster->order_detail_amount = calculate_flat_percent($amount, $serviceStatData['charge']);
+        $orderDetailStoreForChargeForMaster->converted_amount = calculate_flat_percent($converted_amount, $serviceStatData['charge']);
+        $orderDetailStoreForChargeForMaster->order_detail_cause_name = 'charge';
+        $orderDetailStoreForChargeForMaster->notes = 'Wallet Transfer Charge Send to '.$user_name;
+        $orderDetailStoreForChargeForMaster->step = 4;
+        $orderDetailStoreForChargeForMaster->save();
+
+        //For Discount
+        $data->amount = calculate_flat_percent($amount, $serviceStatData['discount']);
+        $data->converted_amount = calculate_flat_percent($converted_amount, $serviceStatData['discount']);
+        $data->order_detail_cause_name = 'discount';
+        $data->notes = 'Wallet Transfer Discount form '.$master_user_name;
+        $data->step = 5;
+        //$data->order_detail_parent_id = $orderDetailStore->getKey();
+        $updateData['order_data']['previous_amount'] = 0;
+        $orderDetailStoreForDiscount = Transaction::orderDetail()->create(Transaction::orderDetail()->orderDetailsDataArrange($data));
+        $orderDetailStoreForDiscountForMaster = $orderDetailStoreForCharge->replicate();
+        $orderDetailStoreForDiscountForMaster->user_id = $data->sender_receiver_id;
+        $orderDetailStoreForDiscountForMaster->sender_receiver_id = $data->user_id;
+        $orderDetailStoreForDiscountForMaster->order_detail_amount = -calculate_flat_percent($amount, $serviceStatData['discount']);
+        $orderDetailStoreForDiscountForMaster->converted_amount = -calculate_flat_percent($converted_amount, $serviceStatData['discount']);
+        $orderDetailStoreForDiscountForMaster->order_detail_cause_name = 'discount';
+        $orderDetailStoreForDiscountForMaster->notes = 'Wallet Transfer Discount to '.$user_name;
+        $orderDetailStoreForDiscountForMaster->step = 6;
+        $orderDetailStoreForDiscountForMaster->save();
+
+        //'Point Transfer Commission Send to ' . $masterUser->name;
+        //'Point Transfer Commission Receive from ' . $receiver->name;
+        return [
+            'previous_amount' => 0,
+            'current_amount' => 0,
+        ];
+
+    }
+
 }
