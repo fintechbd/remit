61ffcea487e349d7a1837246c1b77459d6abbdd7
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-11-28 08:34:23 +0600

    LP-156 generate PHPDoc

--- src/Services/BankTransferService.php
+++ src/Services/BankTransferService.php
@@ -3,14 +3,19 @@
 namespace Fintech\Remit\Services;
 
 use Fintech\Remit\Interfaces\BankTransferRepository;
+use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 
 /**
  * Class BankTransferService
+ * @property BankTransferRepository $bankTransferRepository
  */
 class BankTransferService
 {
     /**
      * BankTransferService constructor.
+     * @param BankTransferRepository $bankTransferRepository
      */
     public function __construct(BankTransferRepository $bankTransferRepository)
     {
@@ -18,45 +23,76 @@ class BankTransferService
     }
 
     /**
-     * @return mixed
+     * @param array $filters
+     * @return Collection|Paginator
      */
-    public function list(array $filters = [])
+    public function list(array $filters = []): Collection|Paginator
     {
         return $this->bankTransferRepository->list($filters);
 
     }
 
-    public function create(array $inputs = [])
+    /**
+     * @param array $inputs
+     * @return Model|\MongoDB\Laravel\Eloquent\Model|null
+     */
+    public function create(array $inputs = []): Model|\MongoDB\Laravel\Eloquent\Model|null
     {
         return $this->bankTransferRepository->create($inputs);
     }
 
-    public function find($id, $onlyTrashed = false)
+    /**
+     * @param $id
+     * @param bool $onlyTrashed
+     * @return Model|\MongoDB\Laravel\Eloquent\Model|null
+     */
+    public function find($id, bool $onlyTrashed = false): Model|\MongoDB\Laravel\Eloquent\Model|null
     {
         return $this->bankTransferRepository->find($id, $onlyTrashed);
     }
 
-    public function update($id, array $inputs = [])
+    /**
+     * @param $id
+     * @param array $inputs
+     * @return Model|\MongoDB\Laravel\Eloquent\Model|null
+     */
+    public function update($id, array $inputs = []): Model|\MongoDB\Laravel\Eloquent\Model|null
     {
         return $this->bankTransferRepository->update($id, $inputs);
     }
 
+    /**
+     * @param $id
+     * @return mixed
+     */
     public function destroy($id)
     {
         return $this->bankTransferRepository->delete($id);
     }
 
+    /**
+     * @param $id
+     * @return mixed
+     */
     public function restore($id)
     {
         return $this->bankTransferRepository->restore($id);
     }
 
-    public function export(array $filters)
+    /**
+     * @param array $filters
+     * @return Paginator|Collection
+     */
+    public function export(array $filters): Paginator|Collection
     {
         return $this->bankTransferRepository->list($filters);
     }
 
-    public function import(array $filters)
+    /**
+     * @param array $filters
+     * @return Model|\MongoDB\Laravel\Eloquent\Model|null
+     */
+    public function import(array $filters): Model|\MongoDB\Laravel\Eloquent\Model|null
     {
         return $this->bankTransferRepository->create($filters);
     }
