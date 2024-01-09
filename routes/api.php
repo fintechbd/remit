9ad60e58a499bb8a73f33c1e9ed087e54a3e8677
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-01-10 01:23:24 +0600

    assign routes created

--- routes/api.php
+++ routes/api.php
@@ -20,12 +20,15 @@ if (Config::get('fintech.remit.enabled')) {
 
             Route::apiResource('bank-transfers', \Fintech\Remit\Http\Controllers\BankTransferController::class)->except('update', 'destroy');
             //Route::post('bank-transfers/{bank_transfer}/restore', [\Fintech\Remit\Http\Controllers\BankTransferController::class, 'restore'])->name('bank-transfers.restore');
+            Route::post('bank-transfers/{bank_transfer}/assign-vendor', [\Fintech\Remit\Http\Controllers\BankTransferController::class, 'assignVendor'])->name('bank-transfers.assign-vendor');
 
             Route::apiResource('cash-pickups', \Fintech\Remit\Http\Controllers\CashPickupController::class)->except('update', 'destroy');
             //Route::post('cash-pickups/{cash_pickup}/restore', [\Fintech\Remit\Http\Controllers\CashPickupController::class, 'restore'])->name('cash-pickups.restore');
+            Route::post('cash-pickups/{cash_pickup}/assign-vendor', [\Fintech\Remit\Http\Controllers\CashPickupController::class, 'assignVendor'])->name('cash-pickups.assign-vendor');
 
             Route::apiResource('wallet-transfers', \Fintech\Remit\Http\Controllers\WalletTransferController::class)->except('update', 'destroy');
             //Route::post('wallet-transfers/{wallet_transfer}/restore', [\Fintech\Remit\Http\Controllers\WalletTransferController::class, 'restore'])->name('wallet-transfers.restore');
+            Route::post('wallet-transfers/{wallet_transfer}/assign-vendor', [\Fintech\Remit\Http\Controllers\WalletTransferController::class, 'assignVendor'])->name('wallet-transfers.assign-vendor');
 
             Route::post('wallet-verification', \Fintech\Remit\Http\Controllers\WalletVerificationController::class)->name('wallet-verification');
 
