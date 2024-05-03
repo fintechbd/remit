2410c7e66180f50faa0b84dd27ff846d6f5a8fc3
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-05-04 02:34:38 +0600

    islami bank testing fetch balance

--- src/Vendors/IslamiBankApi.php
+++ src/Vendors/IslamiBankApi.php
@@ -266,7 +266,9 @@ class IslamiBankApi implements BankTransfer, OrderQuotation
     {
         $xml_string = $this->xmlGenerate($xml_post_string, $method);
         dump($method.'<br>'.$xml_string);
-        dd(Http::soap($this->apiUrl, $method, $xml_string));
+        $response = Http::soap($this->apiUrl, $method, $xml_string);
+        dd($response->body());
+
         //        $headers = [
         //            'Host: '.parse_url($this->apiUrl, PHP_URL_HOST),
         //            'Content-type: text/xml;charset="utf-8"',
