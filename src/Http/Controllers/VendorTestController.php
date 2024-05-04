9bf754aa2d0a951d6db33c5954f850dd207c07ce
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-05-05 01:46:03 +0600

    paymentType change to account_type

--- src/Http/Controllers/VendorTestController.php
+++ src/Http/Controllers/VendorTestController.php
@@ -33,7 +33,7 @@ class VendorTestController extends Controller
     {
         $vendor = app()->make(\Fintech\Remit\Vendors\IslamiBankApi::class);
         $data['account_number'] = '01614747054';
-        $data['paymentType'] = '7';
+        $data['account_type'] = '7';
         dump($vendor->validateBeneficiaryWallet($data));
     }
 
