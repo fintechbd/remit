497689c7be98dc249312cc8eeabd1fc01d4db2e9
mah-shamim <mah-shamim@users.noreply.github.com>
2024-06-03 20:14:01 +0000

    Fix styling

--- routes/api.php
+++ routes/api.php
@@ -30,7 +30,7 @@ if (Config::get('fintech.remit.enabled')) {
             Route::apiResource('bank-transfers', BankTransferController::class)->except('update', 'destroy');
             Route::group(['prefix' => 'bank-transfers'], function () {
                 Route::post('store-without-insufficient-balance', [BankTransferController::class, 'storeWithoutInsufficientBalance'])
-                ->name('store-without-insufficient-balance');
+                    ->name('store-without-insufficient-balance');
             });
             Route::apiResource('cash-pickups', CashPickupController::class)->except('update', 'destroy');
             Route::group(['prefix' => 'cash-pickups'], function () {
