20cd6d9384693ed452f4cbc3817f3735774ebadb
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-06-29 02:59:32 +0600

    LP-244LP-244-Wallet-To-Bank connection response check

--- src/Vendors/MeghnaBankApi.php
+++ src/Vendors/MeghnaBankApi.php
@@ -15,7 +15,7 @@ class MeghnaBankApi implements BankTransfer, OrderQuotation
      */
     public function __construct()
     {
-        $this->config = config('fintech.remit.providers.islamibank');
+        $this->config = config('fintech.remit.providers.meghnabank');
 
         if ($this->config['mode'] === 'sandbox') {
             $this->apiUrl = $this->config[$this->status]['endpoint'];
@@ -25,6 +25,49 @@ class MeghnaBankApi implements BankTransfer, OrderQuotation
             $this->apiUrl = $this->config[$this->status]['endpoint'];
             $this->status = 'live';
         }
+    }
+
+    /**
+     * Base function that is responsible for interacting directly with the Remit Webservice api to obtain data
+     *
+     * @param  array  $params
+     * @return array
+     *
+     * @throws Exception
+     */
+    public function getData($url, $params = [])
+    {
+        $apiUrl = $this->apiUrl.$url;
+        $apiUrl .= http_build_query($params);
+        Log::info($apiUrl);
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $apiUrl);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+            'Authorization: Credentials '.$this->config[$this->status]['token']]);
+
+        $response = curl_exec($curl);
+        $info = curl_getinfo($curl);
+        $error = curl_error($curl);
+
+        if ($response === false) {
+            Log::info($info);
+            Log::info($error);
+            throw new Exception(curl_error($curl), curl_errno($curl));
+        }
+
+        $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
+        Log::info(json_decode($response, true));
+
+        return [
+            'status' => $status,
+            'response' => json_decode($response, true),
+        ];
+
     }
     public function makeTransfer(array $orderInfo = []): mixed
     {
