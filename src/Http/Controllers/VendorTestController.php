4c1c845bd03da7212f6ee7882fe351b0e47145d5
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-07-08 21:16:54 +0600

    LP-259-Meghna-Bank-API https change to http

--- src/Http/Controllers/VendorTestController.php
+++ src/Http/Controllers/VendorTestController.php
@@ -227,7 +227,7 @@ class VendorTestController extends Controller
                 'bankid' => 'MGBL',
                 'agent' => '14',
             ])
-            ->get('https://uatrmsapi.meghnabank.com.bd/VSLExchangeAPI/Controller/remitEnquiry?queryType=1&confRate=y');
+            ->get('http://uatrmsapi.meghnabank.com.bd/VSLExchangeAPI/Controller/remitEnquiry?queryType=1&confRate=y');
 
         dd($response->json());
     }
