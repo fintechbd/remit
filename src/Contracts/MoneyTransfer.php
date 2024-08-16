e72483d6238eb3ddb630bfba32bf64bdfe9363b6
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-08-17 02:04:35 +0600

    bank transfer interface refactored to money transfer

---
+++ src/Contracts/MoneyTransfer.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Fintech\Remit\Contracts;
+
+use Fintech\Core\Abstracts\BaseModel;
+
+interface MoneyTransfer
+{
+
+    /**
+     * Method to make a request to the remittance service provider
+     * for a quotation of the order. that include charge, fee,
+     * commission and other information related to order.
+     *
+     * @param BaseModel $order
+     * @param string $vendor_slug
+     *
+     * @throws \ErrorException
+     */
+    public function requestQuote(BaseModel $order, string $vendor_slug): mixed;
+
+    /**
+     * Method to make a request to the remittance service provider
+     * for an execution of the order.
+     *
+     * @param BaseModel $order
+     * @param string $vendor_slug
+     *
+     * @throws \ErrorException
+     */
+    public function executeOrder(BaseModel $order, string $vendor_slug): mixed;
+
+    /**
+     * Method to make a request to the remittance service provider
+     * for the progress status of the order.
+     *
+     * @param BaseModel $order
+     * @return mixed
+     * @throws \ErrorException
+     */
+    public function orderStatus(BaseModel $order): mixed;
+
+    /**
+     * Method to make a request to the remittance service provider
+     * for the cancellation of the order.
+     *
+     * @param BaseModel $order
+     * @return mixed
+     * @throws \ErrorException
+     */
+    public function cancelOrder(BaseModel $order): mixed;
+
+    /**
+     * Method to make a request to the remittance service provider
+     * for the amendment of the order.
+     *
+     * @param BaseModel $order
+     * @return mixed
+     * @throws \ErrorException
+     */
+    public function amendmentOrder(BaseModel $order): mixed;
+}
