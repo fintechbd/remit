1d7050ff6bcccb04cad60f3329b96162a978139b
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-08-19 12:28:49 +0600

    response formatted added

--- src/Http/Resources/AssignVendorQuotaResource.php
+++ src/Http/Resources/AssignVendorQuotaResource.php
@@ -14,8 +14,50 @@ class AssignVendorQuotaResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        return match ($request->input('vendor_slug')) {
+            'emqapi' => $this->formatEmqApi($request),
+            'transfast' => $this->formatTransFast($request),
+            'valyou' => $this->formatValyou($request),
+            'agrani' => $this->formatAgrani($request),
+            'citybank' => $this->formatCitybank($request),
+            'islamibank' => $this->formatIslamibank($request),
+            'meghnabank' => $this->formatMeghnabank($request),
+            default => parent::toArray($request),
+        };
+    }
+
+    private function formatAgrani(Request $request): array
+    {
         return parent::toArray($request);
     }
 
+    private function formatEmqApi(Request $request): array
+    {
+        return parent::toArray($request);
+    }
 
+    private function formatTransFast(Request $request): array
+    {
+        return parent::toArray($request);
+    }
+
+    private function formatValyou(Request $request): array
+    {
+        return parent::toArray($request);
+    }
+
+    private function formatCitybank(Request $request): array
+    {
+        return parent::toArray($request);
+    }
+
+    private function formatIslamibank(Request $request): array
+    {
+        return parent::toArray($request);
+    }
+
+    private function formatMeghnabank(Request $request): array
+    {
+        return parent::toArray($request);
+    }
 }
