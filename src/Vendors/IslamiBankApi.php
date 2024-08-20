2335e80421ca14f5c063dd099a34530ed2f46b22
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-08-20 17:48:07 +0600

    order execute added

--- src/Vendors/IslamiBankApi.php
+++ src/Vendors/IslamiBankApi.php
@@ -11,6 +11,84 @@ use Illuminate\Support\Facades\Http;
 
 class IslamiBankApi implements MoneyTransfer, WalletTransfer
 {
+    const ERROR_MESSAGES = [
+        1000 => 'ERROR OTHERS',
+        1001 => 'TRANSACTION REF INVALID',
+        1002 => 'AMOUNT INVALID',
+        1003 => 'ISO CODE INVALID',
+        1004 => 'SWIFT CODE INVALID',
+        1005 => 'NOTE INVALID',
+        1006 => 'SECRET KEY INVALID',
+        1007 => 'PAYMENT TYPE INVALID',
+        1008 => 'IDENTITY TYPE INVALID',
+        1009 => 'IDENTITY DESCRIPTION INVALID',
+        1010 => 'EXCHANGE BR CODE INVALID',
+        1011 => 'ISSUE DATE INVALID',
+        1101 => 'TRANSACTION REF MISSING',
+        1102 => 'AMOUNT MISSING',
+        1103 => 'CURRENCY MISSING',
+        1104 => 'SWIFT CODE MISSING',
+        1105 => 'NOTE MISSING',
+        1106 => 'SECRET KEY MISSING',
+        1107 => 'PAYMENT TYPE MISSING',
+        1108 => 'IDENTITY TYPE MISSING',
+        1109 => 'IDENTITY DESCRIPTION MISSING',
+        1110 => 'EXCHANGE BR CODE MISSING',
+        1111 => 'ISSUE DATE MISSING',
+        1201 => 'BENEFICIARY ACC NO NOT APPLICABLE',
+        1202 => 'BENEFICIARY ROUTING NO NOT APPLICABLE',
+        1301 => 'BENEFICIARY ACC NO NOT FOUND',
+        2001 => 'REMITTER NAME INVALID',
+        2002 => 'REMITTER IDENTIFICATION NO INVALID',
+        2003 => 'REMITTER PHONE NO INVALID',
+        2004 => 'REMITTER ADDRESS INVALID',
+        2101 => 'REMITTER NAME MISSING',
+        2102 => 'REMITTER IDENTIFICATION NO MISSING',
+        2103 => 'REMITTER PHONE NO MISSING',
+        2104 => 'REMITTER ADDRESS MISSING',
+        3001 => 'BENEFICIARY NAME INVALID',
+        3002 => 'BENEFICIARY PASSPORT INVALID',
+        3003 => 'BENEFICIARY PHONE INVALID',
+        3004 => 'BENEFICIARY ADDRESS INVALID',
+        3005 => 'BENEFICIARY ACC NO INVALID',
+        3006 => 'BENEFICIARY ACC TYPE INVALID',
+        3007 => 'BENEFICIARY BANK CODE INVALID',
+        3008 => 'BENEFICIARY BANK NAME INVALID',
+        3009 => 'BENEFICIARY BRANCH CODE INVALID',
+        3010 => 'BENEFICIARY BRANCH NAME INVALID',
+        3011 => 'BENEFICIARY ROUTING NO INVALID',
+        3101 => 'BENEFICIARY NAME MISSING',
+        3102 => 'BENEFICIARY PASSPORT MISSING',
+        3103 => 'BENEFICIARY PHONE MISSING',
+        3104 => 'BENEFICIARY ADDRESS MISSING',
+        3105 => 'BENEFICIARY ACC NO MISSING',
+        3106 => 'BENEFICIARY ACC TYPE MISSING',
+        3107 => 'BENEFICIARY BANK CODE MISSING',
+        3108 => 'BENEFICIARY BANK NAME MISSING',
+        3109 => 'BENEFICIARY BRANCH CODE MISSING',
+        3110 => 'BENEFICIARY BRANCH NAME MISSING',
+        3111 => 'BENEFICIARY ROUTING NO MISSING',
+        3112 => 'BENEFICIARY CARD NO MISSING',
+        3113 => 'BENEFICIARY WALLET ACC NO MISSING',
+        3114 => 'BENEFICIARY ACC NO LENGTH INVALID',
+        5001 => 'REMITTANCE ALREADY IMPORTED',
+        5002 => 'REMITTANCE VERIFIED SUCCESSFULLY',
+        5003 => 'REMITTANCE SUCCESS',
+        5004 => 'REMITTANCE FAILED',
+        5005 => 'REMITTANCE SKIPPED',
+        5006 => 'REMITTANCE NOT_FOUND',
+        5007 => 'REMITTANCE IS ENQUEUED',
+        6001 => 'INSUFFICIENT BALANCE',
+        6002 => 'ACCOUNT NAME AND ACCOUNT NO. DIFFER',
+        6003 => 'FIELD LENGTH INVALID',
+        6004 => 'ACCOUNT NO. NOT FOUND',
+        7001 => 'USER NAME OR PASSWORD IS MISSING',
+        7002 => 'USER NAME OR PASSWORD IS INVALID',
+        7003 => 'USER IS BLOCKED',
+        7004 => 'USER IS INACTIVE',
+        7005 => 'USER IS DEAD (PERMANENTLY BLOCKED)',
+    ];
+
     /**
      * IslamiBank API configuration.
      *
@@ -132,92 +210,6 @@ class IslamiBankApi implements MoneyTransfer, WalletTransfer
         //        return $return;
     }
 
-    /**
-     * Response Code List
-     * These codes will return in all operations.
-     */
-    private function __responseCodeList(int|string $code): string
-    {
-        $return = [
-            1000 => 'ERROR OTHERS',
-            1001 => 'TRANSACTION REF INVALID',
-            1002 => 'AMOUNT INVALID',
-            1003 => 'ISO CODE INVALID',
-            1004 => 'SWIFT CODE INVALID',
-            1005 => 'NOTE INVALID',
-            1006 => 'SECRET KEY INVALID',
-            1007 => 'PAYMENT TYPE INVALID',
-            1008 => 'IDENTITY TYPE INVALID',
-            1009 => 'IDENTITY DESCRIPTION INVALID',
-            1010 => 'EXCHANGE BR CODE INVALID',
-            1011 => 'ISSUE DATE INVALID',
-            1101 => 'TRANSACTION REF MISSING',
-            1102 => 'AMOUNT MISSING',
-            1103 => 'CURRENCY MISSING',
-            1104 => 'SWIFT CODE MISSING',
-            1105 => 'NOTE MISSING',
-            1106 => 'SECRET KEY MISSING',
-            1107 => 'PAYMENT TYPE MISSING',
-            1108 => 'IDENTITY TYPE MISSING',
-            1109 => 'IDENTITY DESCRIPTION MISSING',
-            1110 => 'EXCHANGE BR CODE MISSING',
-            1111 => 'ISSUE DATE MISSING',
-            1201 => 'BENEFICIARY ACC NO NOT APPLICABLE',
-            1202 => 'BENEFICIARY ROUTING NO NOT APPLICABLE',
-            1301 => 'BENEFICIARY ACC NO NOT FOUND',
-            2001 => 'REMITTER NAME INVALID',
-            2002 => 'REMITTER IDENTIFICATION NO INVALID',
-            2003 => 'REMITTER PHONE NO INVALID',
-            2004 => 'REMITTER ADDRESS INVALID',
-            2101 => 'REMITTER NAME MISSING',
-            2102 => 'REMITTER IDENTIFICATION NO MISSING',
-            2103 => 'REMITTER PHONE NO MISSING',
-            2104 => 'REMITTER ADDRESS MISSING',
-            3001 => 'BENEFICIARY NAME INVALID',
-            3002 => 'BENEFICIARY PASSPORT INVALID',
-            3003 => 'BENEFICIARY PHONE INVALID',
-            3004 => 'BENEFICIARY ADDRESS INVALID',
-            3005 => 'BENEFICIARY ACC NO INVALID',
-            3006 => 'BENEFICIARY ACC TYPE INVALID',
-            3007 => 'BENEFICIARY BANK CODE INVALID',
-            3008 => 'BENEFICIARY BANK NAME INVALID',
-            3009 => 'BENEFICIARY BRANCH CODE INVALID',
-            3010 => 'BENEFICIARY BRANCH NAME INVALID',
-            3011 => 'BENEFICIARY ROUTING NO INVALID',
-            3101 => 'BENEFICIARY NAME MISSING',
-            3102 => 'BENEFICIARY PASSPORT MISSING',
-            3103 => 'BENEFICIARY PHONE MISSING',
-            3104 => 'BENEFICIARY ADDRESS MISSING',
-            3105 => 'BENEFICIARY ACC NO MISSING',
-            3106 => 'BENEFICIARY ACC TYPE MISSING',
-            3107 => 'BENEFICIARY BANK CODE MISSING',
-            3108 => 'BENEFICIARY BANK NAME MISSING',
-            3109 => 'BENEFICIARY BRANCH CODE MISSING',
-            3110 => 'BENEFICIARY BRANCH NAME MISSING',
-            3111 => 'BENEFICIARY ROUTING NO MISSING',
-            3112 => 'BENEFICIARY CARD NO MISSING',
-            3113 => 'BENEFICIARY WALLET ACC NO MISSING',
-            3114 => 'BENEFICIARY ACC NO LENGTH INVALID',
-            5001 => 'REMITTANCE ALREADY IMPORTED',
-            5002 => 'REMITTANCE VERIFIED SUCCESSFULLY',
-            5003 => 'REMITTANCE SUCCESS',
-            5004 => 'REMITTANCE FAILED',
-            5005 => 'REMITTANCE SKIPPED',
-            5006 => 'REMITTANCE NOT_FOUND',
-            5007 => 'REMITTANCE IS ENQUEUED',
-            6001 => 'INSUFFICIENT BALANCE',
-            6002 => 'ACCOUNT NAME AND ACCOUNT NO. DIFFER',
-            6003 => 'FIELD LENGTH INVALID',
-            6004 => 'ACCOUNT NO. NOT FOUND',
-            7001 => 'USER NAME OR PASSWORD IS MISSING',
-            7002 => 'USER NAME OR PASSWORD IS INVALID',
-            7003 => 'USER IS BLOCKED',
-            7004 => 'USER IS INACTIVE',
-            7005 => 'USER IS DEAD (PERMANENTLY BLOCKED)',
-        ];
-
-        return $return[$code];
-    }
 
     private function __transferData(array $data): array
     {
@@ -721,7 +713,18 @@ class IslamiBankApi implements MoneyTransfer, WalletTransfer
         $wsMessage->appendChild($this->xml->createElement('xsd:transReferenceNo', $data['transReferenceNo'] ?? '?'));
         $service->appendChild($wsMessage);
 
-        return $this->callApi($method, $service);
+        $response = $this->callApi($method, $service);
+
+        $orderInfo = json_decode(
+            preg_replace(
+                '/(.+)\|([0-9+]+)/',
+                '{"status":"$1", "code":$2, "origin_message":"$0"}',
+                $response),
+            true);
+
+        $orderInfo['message'] = isset($orderInfo['code']) ? self::ERROR_MESSAGES[$orderInfo['code']] : $response;
+
+        return $orderInfo;
     }
 
     /**
