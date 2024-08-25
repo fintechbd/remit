2d4b4fd0ff74ab10a497244bc55cf7941a808fd5
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-08-25 19:45:40 +0600

    commission value margined

--- database/seeders/RemitSeeder.php
+++ database/seeders/RemitSeeder.php
@@ -120,7 +120,7 @@ class RemitSeeder extends Seeder
                             'local_currency_higher_limit' => '25000.00',
                             'charge' => mt_rand(1, 7).'%',
                             'discount' => mt_rand(1, 7).'%',
-                            'commission' => mt_rand(1, 7).'%',
+                            'commission' => '0',
                             'cost' => '0.00',
                             'charge_refund' => 'yes',
                             'discount_refund' => 'yes',
