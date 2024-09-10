92774a917e2edc98c0fa9317c55252be6279a1b1
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-09-10 15:29:17 +0600

    missing root prefix issue fixed

--- config/remit.php
+++ config/remit.php
@@ -29,7 +29,7 @@ return [
     | Note: while adding prefix add closing ending slash '/'
     */
 
-    'root_prefix' => 'test/',
+    'root_prefix' => 'api/',
 
     /*
     |--------------------------------------------------------------------------
