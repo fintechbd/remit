aa66511088b88d2dd2bae10b5285a0aa35027bd7
hafijul233 <hafijul233@users.noreply.github.com>
2024-09-19 20:17:05 +0000

    Fix styling

--- src/Remit.php
+++ src/Remit.php
@@ -13,32 +13,32 @@ class Remit
      * @return BankTransferService
      */
     public function bankTransfer($filters = null)
-{
-	return \singleton(BankTransferService::class, $filters);
+    {
+        return \singleton(BankTransferService::class, $filters);
     }
 
     /**
      * @return CashPickupService
      */
     public function cashPickup($filters = null)
-{
-	return \singleton(CashPickupService::class, $filters);
+    {
+        return \singleton(CashPickupService::class, $filters);
     }
 
     /**
      * @return WalletTransferService
      */
     public function walletTransfer($filters = null)
-{
-	return \singleton(WalletTransferService::class, $filters);
+    {
+        return \singleton(WalletTransferService::class, $filters);
     }
 
     /**
      * @return AssignVendorService
      */
     public function assignVendor($filters = null)
-{
-	return \singleton(AssignVendorService::class, $filters);
+    {
+        return \singleton(AssignVendorService::class, $filters);
     }
 
     //** Crud Service Method Point Do not Remove **//
