b1f0e6f1c67fef7f228b0e3ccabeadb1f1ab0f3a
hafijul233 <hafijul233@users.noreply.github.com>
2024-09-24 19:50:18 +0000

    Fix styling

--- src/Events/RemitTransferRequested.php
+++ src/Events/RemitTransferRequested.php
@@ -3,9 +3,7 @@
 namespace Fintech\Remit\Events;
 
 use Fintech\Core\Abstracts\BaseModel;
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
@@ -15,12 +13,6 @@ class RemitTransferRequested
 
     /**
      * Create a new event instance.
-     *
-     * @param string $transferType
-     * @param BaseModel $transfer
      */
-    public function __construct(public string $transferType, public BaseModel $transfer)
-    {
-    }
-
+    public function __construct(public string $transferType, public BaseModel $transfer) {}
 }
