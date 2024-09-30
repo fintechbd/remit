52edf25f32b108d918188ba4e03c213cacf0e03b
hafijul233 <hafijul233@users.noreply.github.com>
2024-09-30 10:24:42 +0000

    Fix styling

--- src/Http/Requests/StoreWalletTransferRequest.php
+++ src/Http/Requests/StoreWalletTransferRequest.php
@@ -47,6 +47,7 @@ class StoreWalletTransferRequest extends FormRequest
     {
         $this->mergeIfMissing(['order_data.request_from' => request()->platform()->value]);
     }
+
     /**
      * Get the validation attributes that apply to the request.
      *
