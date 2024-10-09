6c7a89688c1796b9b6359b1ae956902dec778c76
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-10-10 04:10:32 +0600

    typo fixed

--- src/Traits/CompliancePolicyTable.php
+++ src/Traits/CompliancePolicyTable.php
@@ -10,10 +10,13 @@ trait CompliancePolicyTable
     {
         $policies = [];
 
-        if (! empty($order_data['compliance_data'])) {
+        if (!empty($order_data['compliance_data'])) {
             foreach ($order_data['compliance_data'] as $index => $compliance) {
                 $policies[$index] = $compliance;
                 $policies[$index]['risk'] = RiskProfile::from($compliance['risk']);
+                if (isset($compliance['priority']) && is_array($compliance['priority'])) {
+                    $compliance['priority'] = $compliance['priority']['value'];
+                }
                 $policies[$index]['priority'] = RiskProfile::from($compliance['priority'] ?? 'green');
                 $policies[$index]['remarks'] = $compliance['remarks'] ?? 'N/A';
             }
