d606a6cbd942e732f824fd1ce590e76f1afe4f89
hafijul233 <hafijul233@users.noreply.github.com>
2024-10-09 07:47:16 +0000

    Fix styling

--- src/Traits/CompliancePolicyTable.php
+++ src/Traits/CompliancePolicyTable.php
@@ -6,12 +6,11 @@ use Fintech\Core\Enums\Auth\RiskProfile;
 
 trait CompliancePolicyTable
 {
-
-    public function renderPolicyData(array &$order_data):void
+    public function renderPolicyData(array &$order_data): void
     {
         $policies = [];
 
-        if (!empty($order_data['compliance_data'])) {
+        if (! empty($order_data['compliance_data'])) {
             foreach ($order_data['compliance_data'] as $index => $compliance) {
                 $policies[$index] = $compliance;
                 $policies[$index]['risk'] = RiskProfile::from($compliance['risk']);
@@ -20,5 +19,4 @@ trait CompliancePolicyTable
 
         $order_data['compliance_data'] = $policies;
     }
-
 }
