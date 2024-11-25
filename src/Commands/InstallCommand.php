63e56136d3d9581a50ea5b9d56823a329e06bcc5
hafijul233 <hafijul233@users.noreply.github.com>
2024-11-25 11:03:00 +0000

    Fix styling

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -18,9 +18,9 @@ class InstallCommand extends Command
 
     private string $module = 'Remit';
 
-    private string $image_svg = __DIR__ . '/../../resources/img/service_type/logo_svg/';
+    private string $image_svg = __DIR__.'/../../resources/img/service_type/logo_svg/';
 
-    private string $image_png = __DIR__ . '/../../resources/img/service_type/logo_png/';
+    private string $image_png = __DIR__.'/../../resources/img/service_type/logo_png/';
 
     public function handle(): int
     {
@@ -30,7 +30,7 @@ class InstallCommand extends Command
 
             $this->addDefaultServiceTypes();
 
-//            $this->addSchedulerTasks();
+            //            $this->addSchedulerTasks();
 
         });
 
