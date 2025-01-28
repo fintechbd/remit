386024b9adc69bfc831b1e9ce9950e7c591fa885
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-01-28 16:13:47 +0600

    configuration key optimization for CICD

--- config/remit.php
+++ config/remit.php
@@ -16,7 +16,7 @@ return [
     |--------------------------------------------------------------------------
     | this setting enable the api will be available or not
     */
-    'enabled' => env('PACKAGE_REMIT_ENABLED', true),
+    'enabled' => env('REMIT_ENABLED', true),
 
     'attempt_threshold' => 5,
 
@@ -77,13 +77,13 @@ return [
             'countries' => [],
             'live' => [
                 'endpoint' => 'https://partner.emq.com/v1',
-                'username' => env('PACKAGE_REMIT_EMQ_API_USERNAME'),
-                'password' => env('PACKAGE_REMIT_EMQ_API_PASSWORD'),
+                'username' => env('REMIT_EMQ_API_USERNAME'),
+                'password' => env('REMIT_EMQ_API_PASSWORD'),
             ],
             'sandbox' => [
                 'endpoint' => 'https://sandbox-partner.emq.com/v1',
-                'username' => env('PACKAGE_REMIT_EMQ_API_USERNAME'),
-                'password' => env('PACKAGE_REMIT_EMQ_API_PASSWORD'),
+                'username' => env('REMIT_EMQ_API_USERNAME'),
+                'password' => env('REMIT_EMQ_API_PASSWORD'),
             ],
         ],
         'transfast' => [
@@ -93,11 +93,11 @@ return [
             'countries' => [],
             'live' => [
                 'endpoint' => 'https://send.transfast.ws/api/',
-                'token' => env('PACKAGE_REMIT_TRANS_FAST_TOKEN'),
+                'token' => env('REMIT_TRANS_FAST_TOKEN'),
             ],
             'sandbox' => [
                 'endpoint' => 'https://demo-api.transfast.net/api/',
-                'token' => env('PACKAGE_REMIT_TRANS_FAST_TOKEN'),
+                'token' => env('REMIT_TRANS_FAST_TOKEN'),
             ],
         ],
         'valyou' => [
@@ -107,17 +107,17 @@ return [
             'countries' => [],
             'live' => [
                 'endpoint' => 'https://www.prabhucashsystem.com/SendAPI/webService.asmx',
-                'username' => env('PACKAGE_REMIT_VALYOU_USERNAME'),
-                'password' => env('PACKAGE_REMIT_VALYOU_PASSWORD'),
-                'agent_code' => env('PACKAGE_REMIT_VALYOU_AGENT_CODE'),
-                'session_id' => env('PACKAGE_REMIT_VALYOU_AGENT_SESSION_ID'),
+                'username' => env('REMIT_VALYOU_USERNAME'),
+                'password' => env('REMIT_VALYOU_PASSWORD'),
+                'agent_code' => env('REMIT_VALYOU_AGENT_CODE'),
+                'session_id' => env('REMIT_VALYOU_AGENT_SESSION_ID'),
             ],
             'sandbox' => [
                 'endpoint' => 'https://test.valyouremit.com/SendAPI/webService.asmx',
-                'username' => env('PACKAGE_REMIT_VALYOU_USERNAME'),
-                'password' => env('PACKAGE_REMIT_VALYOU_PASSWORD'),
-                'agent_code' => env('PACKAGE_REMIT_VALYOU_AGENT_CODE'),
-                'session_id' => env('PACKAGE_REMIT_VALYOU_AGENT_SESSION_ID'),
+                'username' => env('REMIT_VALYOU_USERNAME'),
+                'password' => env('REMIT_VALYOU_PASSWORD'),
+                'agent_code' => env('REMIT_VALYOU_AGENT_CODE'),
+                'session_id' => env('REMIT_VALYOU_AGENT_SESSION_ID'),
             ],
         ],
         'agrani' => [
@@ -127,14 +127,14 @@ return [
             'countries' => [],
             'live' => [
                 'endpoint' => 'https://fex.agranibank.org/remapiuat',
-                'username' => env('PACKAGE_REMIT_AGRANI_BANK_USERNAME'),
-                'password' => env('PACKAGE_REMIT_AGRANI_BANK_PASSWORD'),
+                'username' => env('REMIT_AGRANI_BANK_USERNAME'),
+                'password' => env('REMIT_AGRANI_BANK_PASSWORD'),
                 'excode' => '7106',
             ],
             'sandbox' => [
                 'endpoint' => 'https://fex.agranibank.org/remapiuat',
-                'username' => env('PACKAGE_REMIT_AGRANI_BANK_USERNAME'),
-                'password' => env('PACKAGE_REMIT_AGRANI_BANK_PASSWORD'),
+                'username' => env('REMIT_AGRANI_BANK_USERNAME'),
+                'password' => env('REMIT_AGRANI_BANK_PASSWORD'),
                 'excode' => '7086',
             ],
         ],
@@ -145,49 +145,49 @@ return [
             'countries' => [],
             'live' => [
                 'endpoint' => 'http://nrbms.thecitybank.com/nrb_api_test/dynamicApi.php?wsdl',
-                'username' => env('PACKAGE_REMIT_CITY_BANK_USERNAME'),
-                'password' => env('PACKAGE_REMIT_CITY_BANK_PASSWORD'),
+                'username' => env('REMIT_CITY_BANK_USERNAME'),
+                'password' => env('REMIT_CITY_BANK_PASSWORD'),
             ],
             'sandbox' => [
                 'endpoint' => 'http://nrbms.thecitybank.com/dynamicApi.php?wsdl',
-                'username' => env('PACKAGE_REMIT_CITY_BANK_USERNAME'),
-                'password' => env('PACKAGE_REMIT_CITY_BANK_PASSWORD'),
+                'username' => env('REMIT_CITY_BANK_USERNAME'),
+                'password' => env('REMIT_CITY_BANK_PASSWORD'),
             ],
         ],
         'islamibank' => [
-            'mode' => env('PACKAGE_REMIT_ISLAMI_BANK_MODE', 'sandbox'),
+            'mode' => env('REMIT_ISLAMI_BANK_MODE', 'sandbox'),
             'driver' => \Fintech\Remit\Vendors\IslamiBankApi::class,
             'wallet_verification' => true,
             'countries' => [19],
             'live' => [
                 'endpoint' => 'https://ibblmtws.islamibankbd.com/ibblmtws/services/ImportFTTMsgWS?wsdl',
-                'username' => env('PACKAGE_REMIT_ISLAMI_BANK_USERNAME'),
-                'password' => env('PACKAGE_REMIT_ISLAMI_BANK_PASSWORD'),
+                'username' => env('REMIT_ISLAMI_BANK_USERNAME'),
+                'password' => env('REMIT_ISLAMI_BANK_PASSWORD'),
             ],
             'sandbox' => [
                 'endpoint' => 'https://ibblmtws.islamibankbd.com/ibblmtws/services/ImportFTTMsgWS?wsdl',
-                'username' => env('PACKAGE_REMIT_ISLAMI_BANK_USERNAME'),
-                'password' => env('PACKAGE_REMIT_ISLAMI_BANK_PASSWORD'),
+                'username' => env('REMIT_ISLAMI_BANK_USERNAME'),
+                'password' => env('REMIT_ISLAMI_BANK_PASSWORD'),
             ],
         ],
         'meghnabank' => [
-            'mode' => env('PACKAGE_REMIT_MEGHNA_BANK_MODE', 'sandbox'),
+            'mode' => env('REMIT_MEGHNA_BANK_MODE', 'sandbox'),
             'driver' => \Fintech\Remit\Vendors\MeghnaBankApi::class,
             'wallet_verification' => false,
             'countries' => [],
             'live' => [
                 'endpoint' => 'https://uatrmsapi.meghnabank.com.bd/VSLExchangeAPI/Controller/',
-                'bankid' => env('PACKAGE_REMIT_MEGHNA_BANK_BANK_ID', null),
-                'agent' => env('PACKAGE_REMIT_MEGHNA_BANK_AGENT', null),
-                'user' => env('PACKAGE_REMIT_MEGHNA_BANK_USERNAME', null),
-                'password' => env('PACKAGE_REMIT_MEGHNA_BANK_PASSWORD', null),
+                'bankid' => env('REMIT_MEGHNA_BANK_BANK_ID', null),
+                'agent' => env('REMIT_MEGHNA_BANK_AGENT', null),
+                'user' => env('REMIT_MEGHNA_BANK_USERNAME', null),
+                'password' => env('REMIT_MEGHNA_BANK_PASSWORD', null),
             ],
             'sandbox' => [
                 'endpoint' => 'https://uatrmsapi.meghnabank.com.bd/VSLExchangeAPI/Controller/',
-                'bankid' => env('PACKAGE_REMIT_MEGHNA_BANK_BANK_ID', null),
-                'agent' => env('PACKAGE_REMIT_MEGHNA_BANK_AGENT', null),
-                'user' => env('PACKAGE_REMIT_MEGHNA_BANK_USERNAME', null),
-                'password' => env('PACKAGE_REMIT_MEGHNA_BANK_PASSWORD', null),
+                'bankid' => env('REMIT_MEGHNA_BANK_BANK_ID', null),
+                'agent' => env('REMIT_MEGHNA_BANK_AGENT', null),
+                'user' => env('REMIT_MEGHNA_BANK_USERNAME', null),
+                'password' => env('REMIT_MEGHNA_BANK_PASSWORD', null),
             ],
         ],
     ],
