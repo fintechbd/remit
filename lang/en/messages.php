6be2c9d9f3eb77fd79a7f423c6b4d70608c5bd69
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-01-30 21:37:44 +0600

    translation updated

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -3,6 +3,6 @@
 return [
     'verification' => [
         'wallet_provider_not_found' => 'No wallet verification provider found for (:wallet).',
-        'provider_missing_method' => ':provider verification provider doesn\'t implement required interface.',
+        'provider_missing_method' => ':provider provider missing required interface.',
     ],
 ];
